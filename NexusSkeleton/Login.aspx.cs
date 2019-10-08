768121d9afddfcb145034c44bbc4c71fb5d2bd84
dstrockis <d.l.strockis@gmail.com>
2019-10-08 13:44:22 -0700

    Adding support for OpenID Connect authentication

--- NexusSkeleton/Login.aspx.cs
+++ NexusSkeleton/Login.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
 
 public partial class Login : System.Web.UI.Page
 {
@@ -14,16 +16,24 @@ public partial class Login : System.Web.UI.Page
 
     protected void btnLogin_Click(object sender, EventArgs e)
     {
-        if(LdapAuthenticate(txtUsername.Text, txtpassword.Text))
+        if (true) // if button is "login with DID" button
         {
-
-             Session["Username"] = txtUsername.Text;
-            Response.Redirect("Default.aspx");
-
+            HttpContext.Current.GetOwinContext().Authentication.Challenge(
+                new AuthenticationProperties { RedirectUri = "/" },
+                OpenIdConnectAuthenticationDefaults.AuthenticationType);
         }
+        else // else handle other buttons the same way you do right now
+        {
+            if (LdapAuthenticate(txtUsername.Text, txtpassword.Text))
+            {
+
+                Session["Username"] = txtUsername.Text;
+                Response.Redirect("Default.aspx");
+
+            }
+        }
+        
     }
-
-
 
     // authenticate against LDAP,  pass in username, password
     protected bool LdapAuthenticate(string username, string password)
